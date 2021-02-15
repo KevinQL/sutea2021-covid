ed155e3ba7aca8db5474779c4bfa3fd8a97090b8
Noely Flores <Noely.flores@intercorpretail.pe>
2021-02-14 22:52:17 -0500

    Add news interfaces

--- views/assistance.php
+++ views/assistance.php
@@ -5,43 +5,45 @@
         include_once('views/modules/cdnsheader.html');
     ?>
     <link rel="stylesheet" href="views/css/home.css">
-    <title>Inscripción</title>
+    <title>Asistencia</title>
 </head>
 <body>
     <?php
         include_once('views/modules/navegacion_inicio.html');
     ?>
     <section class="container-event container">
-        <h2 class="text-center">Registar asistencia</h2>
-        <p class="text-muted pb-4 text-center"></p>
-        <form action="" method="POST">
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-                <label for="email">Documento de identidad</label>
-                <input type="text" class="form-control" id="document" name="assistance_document" placeholder="DNI" autocomplete="off"
-                required 
-                pattern="^[0-9\\s]+$"
-                minLength="8"
-                maxlength = "8">
+        <div class="card card-own p-sm-4 p-md-4">
+            <div class="card-body">
+                <h2>Asistencia del evento</h2>
+                <p class="lead">Descripción u observación adicional</p>
+                <form action="" method="POST" class="d-flex flex-column align-items-start">
+                    <div class="col-12 col-md-9 col-lg-6 mb-3">
+                        <label for="email">Documento de identidad</label>
+                        <input type="text" class="form-control" id="document" name="assistance_document" placeholder="DNI" autocomplete="off"
+                        required 
+                        pattern="^[0-9\\s]+$"
+                        minLength="8"
+                        maxlength = "8">
+                    </div>
+                    <div class="col-12 col-md-9 col-lg-6 mb-3">
+                        <label for="name">Nombre</label>
+                        <input type="text" class="form-control" id="name" name="assistance_name" placeholder="Nombre" autocomplete="off"
+                        required 
+                        pattern="^[A-Za-z0-9\\s]+$"
+                        maxlength = "30">
+                    </div>
+                    <div class="col-12 col-md-9 col-lg-6 mb-3">
+                        <label for="lastName">Apellido</label>
+                        <input type="text" class="form-control" id="lastName" name="assistance_lastName" placeholder="Apellido" autocomplete="off"
+                        required 
+                        pattern="^[A-Za-z0-9\\s]+${1,50}" 
+                        maxlength = "50">
+                    </div>
+                    <input type="submit" id="assistanceSave" class="next btn btn-primary-own mx-2" value="Confirmar"/>
+                </form>
             </div>
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-                <label for="name">Nombre</label>
-                <input type="text" class="form-control" id="name" name="assistance_name" placeholder="Nombre" autocomplete="off"
-                required 
-                pattern="^[A-Za-z0-9\\s]+$"
-                maxlength = "30">
-            </div>
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-                <label for="lastName">Apellido</label>
-                <input type="text" class="form-control" id="lastName" name="assistance_lastName" placeholder="Apellido" autocomplete="off"
-                required 
-                pattern="^[A-Za-z0-9\\s]+${1,50}" 
-                maxlength = "50">
-            </div>
-            <div class="d-flex justify-content-end">
-                <input type="button" id="assistanceClear" class="next btn btn-secondary-own mx-2" value="Cancelar"/>
-                <input type="submit" id="assistanceSave" class="next btn btn-primary-own mx-2" value="Confirmar"/>
-            </div>
-        </form>
+            <img src="views/assets/animation/post-person.svg" alt="" class="form-img d-none d-md-block">
+        </div>
     </section>    
     <?php
             include_once('views/modules/footer.html');
--- views/speakers.php
+++ views/speakers.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Required meta tags -->
+    <?php
+    include_once('views/modules/cdnsheader.html');
+    ?>
+    <link rel="stylesheet" href="views/css/home.css">
     <title>Expositores</title>
 </head>
+
 <body>
-    <section class="bg-white py-10">        
+    <?php
+    include_once('views/modules/navegacion_inicio.html');
+    ?>
+    <section class="container-event container bg-white py-10 ">
         <h2 class="text-center">Título de la ponencia</h2>
-        <p class="text-muted pb-4 text-center">Subtitulo descripción</p>
+        <p class=" pb-4 text-center lead">Subtitulo descripción</p>
         <div class="container">
             <div class="row text-center">
                 <div class="col-lg-4 mb-5 mb-lg-0">
@@ -22,8 +29,8 @@
                 </div>
                 <div class="col-lg-4 mb-5 mb-lg-0">
                     <div class="icon-stack icon-stack-xl bg-gradient-primary-to-secondary text-white mb-4">
-                    <!-- imagen -->
-                    </div>                    
+                        <!-- imagen -->
+                    </div>
                     <h2>Nombre del ponente</h2>
                     <p small>Título, cargo</p>
                     <p class="mb-0">Breve descripción de la ponencia</p>
@@ -39,5 +46,10 @@
             </div>
         </div>
     </section>
+    <?php
+    include_once('views/modules/footer.html');
+    include_once('views/modules/cdnsfooter.html');
+    ?>
 </body>
+
 </html>
