9126fa9d1eb2899ee3881b3d77620daccfb0bdc2
kev22 <unajmakev@gmail.com>
2021-02-21 17:51:47 -0500

    cambios basicos

--- views/page_sutep.php
+++ views/page_sutep.php
@@ -161,10 +161,6 @@
     </div> -->
   </section>
 
-  <section class="container">
-    <iframe width="100%" height="550" src="https://www.youtube.com/embed/Qg0JnO5-OC4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </section>
-
 
   <!-- Sección videos -->
   <section class="bg-light container-event" id="capacitacion">
--- views/transmision.php
+++ views/transmision.php
@@ -16,23 +16,12 @@
     include_once("views/modules/navegacion__.php");
     ?>
 
-    <section id="inicioSutep" class="inicio_sutep">
-        <div class="container text-center d-flex flex-column align-items-center justify-content-center position-absolute content-cover">
-            <div class="col-md-12">
-                <h1>Bienvenido a <span class="font-color-vprimary">CERSUTEA</span> </h1>
-                <h2 class="mb-5">
-                    Frase descripción Frase descripción Frase descripción Frase descripción
-                </h2>
-                <a class="btn  btn-primary-own btn-lg" target="_blank" href="?pg=page_sutep" role="button">Web CERSUTEA 2021</a>
-            </div>
-        </div>
-        <div class="container text-center d-flex align-items-center justify-content-end position-absolute social">
-            <a class="btn social-white mx-1" target="_blank" href="?pg=" role="button"><i class="fab fa-facebook-f"></i></a>
-            <a class="btn social-white mx-1" target="_blank" href="?pg=" role="button"><i class="fab fa-twitter"></i></a>
-            <a class="btn social-white mx-1" target="_blank" href="?pg=" role="button"><i class="fab fa-youtube"></i></a>
-            <a class="btn social-white mx-1" target="_blank" href="?pg=" role="button"><i class="fas fa-envelope"></i></a>
-        </div>
-    </section>
+
+        <section class="container">
+            <iframe width="100%" height="550" src="https://www.youtube.com/embed/Qg0JnO5-OC4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
+
+
     <?php
     include_once('views/modules/cdnsfooter.html');
     ?>
