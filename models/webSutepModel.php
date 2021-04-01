d28fa972b7827d422548cc41467fd1d570d35bb0
kevQL <unajmakev@gmail.com>
2021-04-01 12:24:59 -0500

    implementado certificado digital

--- models/webSutepModel.php
+++ models/webSutepModel.php
@@ -7,7 +7,75 @@
         require_once "./models/mainModel.php";
     }
 
-    class webSutepModel extends mainModel{        
+    class webSutepModel extends mainModel{     
+        
+        protected function obtenerRutaCertificado_Model($data){
+            $query = "SELECT * FROM certificado c WHERE c.evento_idevento = {$data->evento_idevento} AND c.tipo_persona_idtipo_persona = {$data->tipo_persona_idtipo_persona}";
+            $result = mainModel::ejecutar_una_consulta($query);
+            
+            $eval = false;
+            $data = [];
+
+            if($result->rowCount() >= 1){
+                while($user_fla = $result->fetch(PDO::FETCH_ASSOC)){
+                    //
+                    $data = $user_fla;
+                    $eval = true;
+                }             
+            }
+            return ['eval'=>$eval, 'data'=>$data];
+        }
+
+
+        /**
+         *  Función que se utiliza en la automatizacion de PDF's 
+         */
+        protected function obtenerDocente_Model($data){
+            $query = "SELECT r.anio, r.estado, r.evento_idevento, d.dni, d.nombre, d.apellido, d.especialidad, tp.idtipo_persona, tp.detalle as tipo_persona 
+                FROM registro r 
+                INNER JOIN decente d 
+                ON r.decente_iddecente = d.iddecente
+                INNER JOIN tipo_persona tp
+                ON tp.idtipo_persona = d.tipo_persona_idtipo_persona
+                WHERE r.anio = {$data->anio}
+                AND d.dni = '{$data->dni}'
+            ";
+            $result = mainModel::ejecutar_una_consulta($query);
+            
+            $eval = false;
+            $data = [];
+
+            if($result->rowCount() >= 1){
+                while($user_fla = $result->fetch(PDO::FETCH_ASSOC)){
+                    //verifica que la contrasenia sea correspondida
+                    $data = $user_fla;
+                    $eval = true;
+                }             
+            }
+            return ['eval'=>$eval, 'data'=>$data];
+        }
+
+
+        /**
+         *  Función que se utiliza en la automatizacion de PDF's 
+         */
+        protected function obtenerPonente_Model($data){
+            $query = "SELECT * FROM ponente p WHERE p.dni='{$data->dni}' AND p.anio={$data->anio} AND p.estado=1";
+            $result = mainModel::ejecutar_una_consulta($query);
+            
+            $eval = false;
+            $data = [];
+
+            if($result->rowCount() >= 1){
+                while($user_fla = $result->fetch(PDO::FETCH_ASSOC)){
+                    //verifica que la contrasenia sea correspondida
+                    $data = $user_fla;
+                    $eval = true;
+                }             
+            }
+            return ['eval'=>$eval, 'data'=>$data];
+        }
+
 
         /**
          * 
--- views/certificado_digital.php
+++ views/certificado_digital.php
@@ -4,76 +4,120 @@
 require_once('./public/lib/fpdf/fpdf.php');
 require_once("./public/lib/phpqrcode/qrlib.php");
 
-//Metodos de la clase controlador
-// require_once("./controllers/adminController.php");
-
-$objPDF = new adminController();
-
-$msj_test = $objPDF->test_Controller_msj();
-
 //VARIABLES OBTENIDAS
 $dni = $_GET['code'];
 $anio = $_GET['anio'];
 
+//filtrar y limpiar los datos para ataques de inyeccion sql
+
 //VARIABLES GLOBALES
-$nombre = "ING. LENYN ELÍ FLORES BALANDRA $msj_test";
-$nombre = utf8_decode($nombre);
-$nombre = strtoupper($nombre);
-
-$nivel = "ingeniero"; $nivel = strtoupper(utf8_decode($nivel));
-$tipo_persona = "ponente"; $tipo_persona = strtoupper(utf8_decode($tipo_persona));
-
-$tipo_certificado = 0;
-
-
-//consulta si existe el usuario con el tipo y anio de evento
+$nombre = "";
+$nivel = "";
+$tipo_persona = "";
 
 //Imagen QR
-QRcode::png($dni, "test.png");
+//QRcode::png($dni, "test.png"); // Esto parece innecesario. asi que se puede comentar para que eventualmente se elimine. 
+
+//Instancia objeto controlador. Para resolver metodos necesarios para desarrollar la automatización de los certificados.
+$objPDF = new webSutepController();
+
+$nombre = txt_certi($nombre);
+
+$eval_certi = true; //Controla si el registro existe en la base de datos. 
+
+$res_ponente = $objPDF->obtenerPonente_Controller($dni,$anio); // Se filtran los datos en la tabla ponente. 
+
+//Es ponente? En el caso de que exista 
+if($res_ponente['eval']){
+    $ponente = $res_ponente['data'];
+
+    $nombre = txt_certi($ponente['nombre']) . ' ' . txt_certi($ponente['apellido']);
+    // $nivel = txt_certi($ponente['especialidad']);
+    $nivel = txt_certi('especialista');
+    $tipo_persona = txt_certi('ponente');
+    $fondo_certi = $objPDF->obtenerRutaCertificado_Controller($ponente['evento_idevento'], 3); //"2021/cert3.jpg". El 3 significa que es de tipo persona ponente (id de tabla).
+}else{
+    //es asistente?
+    $res_usuario = $objPDF->obtenerDocente_Controller($dni, $anio);
+    if($res_usuario['eval']){
+        $usuario = $res_usuario['data'];
+
+        $nombre = txt_certi($usuario['nombre']) . ' ' . txt_certi($usuario['apellido']);
+        $nivel = txt_certi($usuario['especialidad']);
+        $tipo_persona = txt_certi($usuario['tipo_persona']);
+        $fondo_certi = $objPDF->obtenerRutaCertificado_Controller($usuario['evento_idevento'], $usuario['idtipo_persona']); //"2021/cert#.jpg"
+    }else{
+        //El registro no existe en la base de datos
+        $eval_certi = false;
+    }
+}
+
+if($eval_certi){
+    
+    // var_dump($res_ponente);
+    // echo "<br>";
+    // echo "<br>";
+    // var_dump($res_usuario);
+
+    //INICIANDO CON LA HOJA EN BLANCO
+    $pdf = new FPDF('L','mm','A4');
+    $pdf->AddPage();
+
+    // IMAGEN FONDO DEL CERTIFICADO
+    $pdf->Image("./public/images_certi/{$fondo_certi}", 0, 0, 300, 211);
+
+    // CODE QR DEL DNI EN ELCERTIFICADO
+    // $pdf->Image("test.png", 28, 17, 35, 35,"png");
+    // $pdf->Image("http://localhost/sutea2021-covid/?pg=certificado/test-qr&code=$dni", 28, 17, 35, 35,"png");
+    $pdf->Image("https://cersutea.com/cersutea/?pg=certificado/test-qr&code=$dni", 28, 17, 35, 35, "png");
+
+    //NOMBRE DEL USUARIO CERTIFICADO
+    $pdf->SetFont('Arial','B',16);
+    $pdf->SetY(80);
+    $pdf->Cell(63);
+    $pdf->Cell(160, 9, $nombre, 0, 1, 'C');
+
+    //NIVEL DEL USUARIO CERTIFICADO
+    $pdf->SetFont('Arial','B',14);
+    $pdf->SetY(94);
+    $pdf->Cell(84);
+    $pdf->Cell(51, 7, $nivel, 0, 1, 'C');
+
+    //TIPO DEL USUARIO CERTIFICADO
+    $pdf->SetFont('Arial','B',14);
+    $pdf->SetY(94);
+    $pdf->Cell(220);
+    $pdf->Cell(51, 7, $tipo_persona, 0, 1, 'C');
+
+    // LINK DE REFERENCIA DEL CERTIFICADO
+    $pdf->SetFont('Times','B',8);
+    $pdf->Text(10, 206, "Verificar certificado digital en ");
+    $pdf->SetTextColor(0,0,255);
+    $pdf->SetFont('','U');
+    $pdf->Text(48, 206, "https://cersutea.com/cersutea/?pg=certification");
 
 
-//INICIANDO CON LA HOJA EN BLANCO
-$pdf = new FPDF('L','mm','A4');
-$pdf->AddPage();
+    $pdf->Output();
 
-// IMAGEN FONDO DEL CERTIFICADO
-$pdf->Image('./public/images_certi/2021/cert1.jpg',0,0,300,211);
 
-// CODE QR DEL DNI EN ELCERTIFICADO
-// $pdf->Image("test.png", 28, 17, 35, 35,"png");
-// $pdf->Image("http://localhost/sutea2021-covid/?pg=certificado/test-qr&code=$dni", 28, 17, 35, 35,"png");
-$pdf->Image("https://cersutea.com/cersutea/?pg=certificado/test-qr&code=$dni", 28, 17, 35, 35,"png");
-
-//NOMBRE DEL USUARIO CERTIFICADO
-$pdf->SetFont('Arial','B',16);
-$pdf->SetY(80);
-$pdf->Cell(63);
-$pdf->Cell(160, 9, $nombre, 0, 1, 'C');
-
-//NIVEL DEL USUARIO CERTIFICADO
-$pdf->SetFont('Arial','B',14);
-$pdf->SetY(94);
-$pdf->Cell(84);
-$pdf->Cell(51, 7, $nivel, 0, 1, 'C');
-
-//TIPO DEL USUARIO CERTIFICADO
-$pdf->SetFont('Arial','B',14);
-$pdf->SetY(94);
-$pdf->Cell(220);
-$pdf->Cell(51, 7, $tipo_persona, 0, 1, 'C');
-
-//NOMRE DE LA PERSONA CERTIFICADA.
-// $pdf->Text(75, 80, "kevin quispe lima");
-
-// LINK DE REFERENCIA DEL CERTIFICADO
-$pdf->SetFont('Times','B',8);
-$pdf->Text(10, 206, "Verificar certificado digital en ");
-$pdf->SetTextColor(0,0,255);
-$pdf->SetFont('','U');
-$pdf->Text(48, 206, "https://cersutea.com/cersutea/?pg=certification");
+}else{
+    // En el caso de que no exista el registro en la base de datos.Hacer lo siguiente.
+    echo "NO TIENE CERTIFICADO!! Comuniquese con el administrador para verificar el estado de su inscripción.";
+}
 
 
 
-$pdf->Output();
+
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+
+/**
+ * FUNCION PARA CORREGIR TILDE Y PONER EN MAYUSCULA EL TEXTO QUE SE ENVIÉ POR PARAMETRO.
+ */
+function txt_certi($txt){
+    return utf8_decode(strtoupper($txt));
+}
+
 
 ?>
