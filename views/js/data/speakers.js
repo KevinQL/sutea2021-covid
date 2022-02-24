d60268d863fa13d4260dd8f58190633f37e7696f
lenyn <lenyn.unajma.18@gmail.com>
2022-02-24 12:25:24 -0500

    cambios en ponentes

--- views/js/data/speakers.js
+++ views/js/data/speakers.js
@@ -1,22 +1,22 @@
 export const speakers = {
     "title": "Conferencistas",
-    "subtitle": "2021",
+    "subtitle": "2022",
     "events": [
         {
             "day": "Lunes",
-            "date": "22 de febreroro del 2021",
+            "date": "28 de febreroro del 2022",
             "speakers": [
                 {
-                    "name": "Prof. Moisés Chipana Valencia",
-                    "proffesion": "Dirigente Nacional de FENATEPERU",
-                    "description": "Sindicalismo Clasista (principios del sindicalismo) luchas y perspectivas",
+                    "name": "Prof. --",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "10:00 - 12:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Dr. Alex Alberto Chaman Portugal",
-                    "proffesion": "Docente UMSA UPEA Bolivia Docente de Post Grado",
-                    "description": "Alcances de la Filosofía de la Educación en tiempos del Neoliberalismo.",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "-----",
                     "date": "15:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -25,26 +25,26 @@ export const speakers = {
         },
         {
             "day": "Martes",
-            "date": "23 de febrero del 2021",
+            "date": "01 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Geraldine Coloti",
-                    "proffesion": "Periodista y escritora italiana",
-                    "description": "La manipulación mediática y la desinformación",
+                    "name": "--",
+                    "proffesion": "--",
+                    "description": "----",
                     "date": "09:00 - 11:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Maestro Carlos Gallardo",
-                    "proffesion": "Uno de los fundadores del SUTEP 1972",
-                    "description": "Historia de las luchas magisteriales y sus repercusiones.",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "11:00 - 13:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "MSc María Carmen Romano",
-                    "proffesion": "Docente e investigadora de la Universidad de Buenos Aires. (Argentina)",
-                    "description": "La Educación en Época de COVID 19.",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "15:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -53,26 +53,26 @@ export const speakers = {
         },
         {
             "day": "Miercoles",
-            "date": "24 de febrero del 2021",
+            "date": "02 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "MSc. Doris Alexandra",
-                    "proffesion": "Colegio de Profesores de Chile",
-                    "description": "Análisis de la realidad educativa de América latina en el marco de la lucha por una Nueva Constitución Política",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "08:00 - 10:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Dr. Juan Melchor Román",
-                    "proffesion": "México Representante Sindical de México",
-                    "description": "Educación Mexicana y la lucha contra el sistema neoliberal",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "10:00 - 13:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Dr. Julio Miguel Reza Huaroc",
-                    "proffesion": "Laboralista Internacional",
-                    "description": "Procesos administrativos en el magisterio.",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "15:00 - 17:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -82,33 +82,33 @@ export const speakers = {
         },
         {
             "day": "Jueves",
-            "date": "25 de febrero del 2021",
+            "date": "03 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Hernán Rodríguez Antón",
-                    "proffesion": "Consultor del MINEDU",
-                    "description": "Estrategia para el Pensamiento Crítico Y Creativo",
+                    "name": "--",
+                    "proffesion": "--",
+                    "description": "----",
                     "date": "08:00 - 10:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Dr. Hugo Valencia Almonte",
-                    "proffesion": "Docente de la Universidad Nacional del Altiplano",
-                    "description": "Los mitos de las Evaluaciones durante el proceso de aprendizaje",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "10:00 - 13:00",
                     "photo": "views/assets/animation/employee.svg"
                 },
                 {
-                    "name": "Dr. Sigfredo Chiroque Torres",
+                    "name": "--",
                     "proffesion": "---",
-                    "description": "Desarrollo de conciencia crítica e investigación en los estudiantes",
+                    "description": "----",
                     "date": "15:00 - 17:00",
                     "photo": "views/assets/animation/employee.svg"
                 },
                 {
-                    "name": "Evo Morales Ayma",
-                    "proffesion": "Ex presidente de Bolivia",
-                    "description": "Experiencia política en Bolivia Sobre la lucha Neoliberal",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "17:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -117,26 +117,26 @@ export const speakers = {
         },
         {
             "day": "Viernes",
-            "date": "26 de febrero del 2021",
+            "date": "04 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Dr. Julio Miguel Reza Huaroc",
-                    "proffesion": "Laboralista Internacional",
-                    "description": "Legislación Educativa y derechos laborales.",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "08:00 - 10:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Equipo de la DREA por niveles",
-                    "proffesion": "Dirección Regional de Educación Apurímac",
-                    "description": "Programación Curricular con relación a la educación remota",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "10:00 - 13:00",
                     "photo": "views/assets/animation/employee.svg"
                 },
                 {
-                    "name": "Equipo de la DREA por niveles",
-                    "proffesion": "Dirección Regional de Educación Apurímac",
-                    "description": "Programación Curricular",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "15:00 - 19:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -145,19 +145,19 @@ export const speakers = {
         },
         {
             "day": "Sábado",
-            "date": "27 de febrero del 2021",
+            "date": "05 de Marzo del 2022",
             "speakers": [
                 {
-                    "name": "Equipo de la DREA por niveles",
-                    "proffesion": "Dirección Regional de Educación Apurímac",
-                    "description": "Programación Curricular con relación a la educación remota",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "08:00 - 13:00",
                     "photo": "views/assets/animation/employee.svg"
                 },                
                 {
-                    "name": "Equipo de la DREA por niveles",
-                    "proffesion": "Dirección Regional de Educación Apurímac",
-                    "description": "Programación Curricular",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "15:00 - 19:00",
                     "photo": "views/assets/animation/employee.svg"
                 },
@@ -166,12 +166,12 @@ export const speakers = {
         },
         {
             "day": "Lunes",
-            "date": "01 de marzo del 2021",
+            "date": "07 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Mg. Daniel Molina Solís",
+                    "name": "--",
                     "proffesion": "---",
-                    "description": "Razonamiento Matemático Taller para resolver problemas para el examen de nombramiento y ascenso de escala",
+                    "description": "----",
                     "date": "14:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -180,12 +180,12 @@ export const speakers = {
         },
         {
             "day": "Martes",
-            "date": "02 de marzo del 2021",
+            "date": "08 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "DREA",
-                    "proffesion": "Dirección Regional de Educación Apurímac",
-                    "description": "Razonamiento Verbal Taller para resolver problemas para el examen de nombramiento y ascenso de escala",
+                    "name": "--",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "14:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -194,12 +194,12 @@ export const speakers = {
         },
         {
             "day": "Miércoles",
-            "date": "03 de marzo del 2021",
+            "date": "09 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Docentes formadores en EIB Apurímac",
+                    "name": "--",
                     "proffesion": "---",
-                    "description": "EIB Taller: Gramática Quechua",
+                    "description": "-----",
                     "date": "14:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -209,12 +209,12 @@ export const speakers = {
         
         {
             "day": "Jueves",
-            "date": "04 de marzo del 2021",
+            "date": "10 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Docentes formadores en EIB Apurímac",
+                    "name": "--",
                     "proffesion": "---",
-                    "description": "EIB Taller: Gramática Quechua",
+                    "description": "----",
                     "date": "14:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -223,12 +223,12 @@ export const speakers = {
         },
         {
             "day": "Viernes",
-            "date": "05 de marzo del 2021",
+            "date": "11 de marzo del 2022",
             "speakers": [
                 {
-                    "name": "Ing. Lenyn Elí Flores Balandra",
-                    "proffesion": "Especialista en Tecnología de la Información y Comunicación",
-                    "description": "Herramientas digitales y la aplicación en la educación remota (uso adecuado)",
+                    "name": "--",
+                    "proffesion": "----",
+                    "description": "-----",
                     "date": "14:00 - 18:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
@@ -237,12 +237,12 @@ export const speakers = {
         },
         {
             "day": "Sábado",
-            "date": "06 de marzo del 2021",
+            "date": "12 de marzo del 2021",
             "speakers": [
                 {
                     "name": "---",
-                    "proffesion": "Especialista en Tecnología de la Información y Comunicación",
-                    "description": "Herramientas digitales y la aplicación en la educación remota (uso adecuado)",
+                    "proffesion": "---",
+                    "description": "----",
                     "date": "14:00 - 17:00",
                     "photo": "views/assets/animation/employee.svg"
                 }
