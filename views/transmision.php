b29611c7c0f1baffa5e17f8a406457163fefbca8
kev22 <unajmakev@gmail.com>
2022-03-28 17:02:25 -0500

    mensaje para suspender trasmisión

--- views/transmision.php
+++ views/transmision.php
@@ -28,7 +28,19 @@
         </section>
   </section>
   
-
+          
+  <script>
+    let current = new Date();
+    let f2 = new Date(2022, 2, 14, 15, 40); //fech
+    console.log({current, f2})
+    if(current > f2 && false){
+      Swal.fire(
+        'Comunicado',
+        'Por motivos de apertura de año escolar y petición de muchos de nuestros docentes, la capacitación de hoy se suspende, pero los esperamos mañana con la ponencia del ING Lenyn Eli Flores Balandra con el tema: herramientas digitales para el proceso de enseñanza.',
+        'info'
+      )
+    }
+  </script>
 <!-- Pie de página -->
     <?php
         include_once('views/modules/footer.html');
